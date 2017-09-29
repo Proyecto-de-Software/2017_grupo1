1b1df17bb2a72ccbeb23d768c329c6a39054240e
ORTU, AGUSTIN SALVADOR <ortu.agustin@gmail.com>
2017-09-29 14:51:32 -0300

    correccion require_once twig

--- private/autoloader.php
+++ private/autoloader.php
@@ -1,6 +1,7 @@
 <?php
-require_once "/var/www/html/vendor/Twig/lib/Twig/Autoloader.php";
+require_once "./vendor/Twig/lib/Twig/Autoloader.php";
 require_once "./controllers/FrontEndController.php";
 require_once "./controllers/IndexController.php";
 require_once "./views/TwigView.php";
 require_once "./views/IndexView.php";
+?>
