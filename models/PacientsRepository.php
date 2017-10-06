7e5b089ebf11500bad03ca1a0368e1be75e6f940
MIRANDA, CAMILA BELEN <camimiranda95@gmail.com>
2017-10-06 11:55:12 -0300

    modelo de pacientes arreglado

--- models/PacientsRepository.php
+++ models/PacientsRepository.php
@@ -9,12 +9,19 @@ class PacientsRepository extends PDORepository
         $element['id'],
         $element['first_name'],
         $element['last_name'],
+        $element['birth_date'],
         $element['gender'],
-        $element['$doc_type'],
-        $element['$dni'],
+        $element['doc_type'],
+        $element['dni'],
         $element['address'],
         $element['phone'],
-        $element['id_medical_insurance'] );
+        $element['id_medical_insurance'],
+        $element['has_refrigerator'],
+        $element['has_electricity'],
+        $element['has_pet'],
+        $element['home_type'],
+        $element['heating_type'],
+        $element['water_type']);
     }
 
     return $answer;
