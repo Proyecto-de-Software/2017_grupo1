70570ebf317f1157664133e77dcdc9d3298abb47
ORTU, AGUSTIN SALVADOR <ortu.agustin@gmail.com>
2017-10-14 14:25:43 -0300

    Renombro UsersView -> UserListView

--- private/autoloader.php
+++ private/autoloader.php
@@ -36,7 +36,7 @@ require_once "./views/LoginView.php";
 require_once "./views/AdminView.php";
 require_once "./views/DisabledSiteView.php";
 
-require_once "./views/UsersView.php";
+require_once "./views/UserListView.php";
 require_once "./views/UserNewView.php";
 require_once "./views/UserAddedView.php";
 require_once "./views/UserFormView.php";
