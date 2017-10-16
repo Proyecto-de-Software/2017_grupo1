c97d12347ccd997e1b31d69ec6c78b8c975560a8
ORTU, AGUSTIN SALVADOR <ortu.agustin@gmail.com>
2017-10-16 01:15:54 -0300

    En el listado de pacientes tomar del referenceDataService los datos de tablas auxiliares

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ function getFrontEndController()
     $frontEndController->addController('user_destroy', new UserDestroyedController(new UserDestroyedView, $userRepository));
     $frontEndController->addController('user_toggle_status', new UserToggleStatusController($userListController, $userRepository));
 
-    $frontEndController->addController('pacients_index', new PacientListController(new PacientListView, $pacientsRepository,$appConfig));
+    $frontEndController->addController('pacients_index', new PacientListController(new PacientListView($referenceDataService), $pacientsRepository, $appConfig));
     $frontEndController->addController('pacient_new', new PacientNewController(new NewPacientView($referenceDataService), $pacientsRepository));
     $frontEndController->addController('pacient_added', new PacientAddedController(new PacientAddedView, $pacientsRepository));
     $frontEndController->addController('pacient_form_update', new PacientEditController(new EditPacientView($referenceDataService), $pacientsRepository));
