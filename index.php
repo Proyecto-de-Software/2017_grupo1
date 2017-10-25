00947c279a5b924ff6f2584e4eb2eb753d9cd615
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-25 12:28:48 -0300

    muevo todos los archivos a private, menos el index.php

--- index.php
+++ index.php
@@ -6,59 +6,8 @@ error_reporting(-1);
 require_once "./private/autoloader.php";
 
 session_start();
-$router = NULL;
-
-function getRouter()
-{
-  if (!isset($router)) {
-    $appConfig = new AppConfig;
-    $userRepository = new UserRepository($appConfig);
-    $pacientsRepository = new PacientsRepository($appConfig);
-    $referenceDataService = new ReferenceDataService(
-      new WaterTypeRepository,
-      new HeatingTypeRepository,
-      new DocumentTypeRepository,
-      new SocialInsuranceRepository,
-      new HomeTypeRepository
-    );
-
-    $indexView = new IndexView;
-    $loginView = new LoginView;
-    $indexController = new IndexController($indexView);
-
-    TwigView::setAppConfig($appConfig);
-    $router = new Router($appConfig, $userRepository);
-
-    $router->addController('index', $indexController);
-    $router->addController('login', new LoginController($loginView));
-    $router->addController('do-login', new DoLoginController(new $indexView, $loginView, $userRepository));
-    $router->addController('do-logout', new DoLogoutController($indexController));
-    $router->addController('admin', new AdminController(new AdminView));
-    $router->addController('admin_updated', new AdminUpdateController($indexView, $appConfig));
-
-    $userListController = new UserListController(new UserListView, $userRepository, $appConfig);
-    $router->addController('users_index', $userListController);
-    $router->addController('user_new', new UserNewController(new NewUserView, $userRepository));
-    $router->addController('user_added', new UserAddedController(new UserAddedView, $userRepository));
-    $router->addController('user_form_update', new UserFormController(new EditUserView, $userRepository));
-    $router->addController('user_updated', new UserUpdatedController(new UserUpdatedView, $userRepository));
-    $router->addController('user_destroy', new UserDestroyedController(new UserDestroyedView, $userRepository));
-    $router->addController('user_toggle_status', new UserToggleStatusController($userListController, $userRepository));
-
-    $router->addController('pacients_index', new PacientListController(new PacientListView($referenceDataService), $pacientsRepository, $appConfig));
-    $router->addController('pacient_new', new PacientNewController(new NewPacientView($referenceDataService), $pacientsRepository));
-    $router->addController('pacient_added', new PacientAddedController(new PacientAddedView, $pacientsRepository));
-    $router->addController('pacient_form_update', new PacientEditController(new EditPacientView($referenceDataService), $pacientsRepository));
-    $router->addController('pacient_updated', new PacientUpdatedController(new PacientUpdatedView, $pacientsRepository));
-    $router->addController('pacient_destroy', new PacientDestroyedController(new PacientDestroyedView, $pacientsRepository));
-    $router->addController('pacient_demographic_data', new PacientEditController(new PacientDemographicDataView($referenceDataService), $pacientsRepository));
-  }
-
-  return $router;
-}
-
 
 if (isset($_GET['action']))
-  getRouter()->getController($_GET['action'])->showView($_POST);
+  Router::getRouter()->getController($_GET['action'])->showView($_POST);
 else
-  getRouter()->getController('index')->showView($_POST);
+  Router::getRouter()->getController('index')->showView($_POST);
---
+++ private/controllers/Router.php
@@ -0,0 +1,95 @@
+<?php
+class Router
+{
+  private $controllers;
+  private $appConfig;
+  private $disabledSiteController;
+  private $nonAuthorizedController;
+  private $userRepository;
+  private static $router;
+  private static $admin_actions = array('admin', 'admin_updated');
+
+  private function getUserSession()
+  {
+    return new UserSession($this->userRepository);
+  }
+
+  public function __construct($appConfig, $userRepository)
+  {
+    $this->disabledSiteController = new DisabledSiteController;
+    $this->nonAuthorizedController = new NonAuthorizedController;
+    $this->appConfig = $appConfig;
+    $this->controllers = [];
+    $this->userRepository = $userRepository;
+  }
+
+  public function addController($key, $controller)
+  {
+    $this->controllers[$key] = $controller;
+  }
+
+  public function getController($key)
+  {
+    if (!$this->appConfig->getIsSiteEnabled())
+    {
+      if (($this->getUserSession()->hasRole('ADMINISTRADOR')) && (in_array($key, self::$admin_actions)))
+        return $this->controllers[$key];
+      else
+        return $this->disabledSiteController;
+    }
+
+    if (!$this->getUserSession()->hasPermission($key))
+      return $this->nonAuthorizedController;
+
+    return $this->controllers[$key];
+  }
+
+  static function getRouter()
+  {
+    if (!isset(self::$router)) {
+      $appConfig = new AppConfig;
+      $userRepository = new UserRepository($appConfig);
+      $pacientsRepository = new PacientsRepository($appConfig);
+      $referenceDataService = new ReferenceDataService(
+        new WaterTypeRepository,
+        new HeatingTypeRepository,
+        new DocumentTypeRepository,
+        new SocialInsuranceRepository,
+        new HomeTypeRepository
+      );
+
+      $indexView = new IndexView;
+      $loginView = new LoginView;
+      $indexController = new IndexController($indexView);
+
+      TwigView::setAppConfig($appConfig);
+      self::$router = new Router($appConfig, $userRepository);
+
+      self::$router->addController('index', $indexController);
+      self::$router->addController('login', new LoginController($loginView));
+      self::$router->addController('do-login', new DoLoginController(new $indexView, $loginView, $userRepository));
+      self::$router->addController('do-logout', new DoLogoutController($indexController));
+      self::$router->addController('admin', new AdminController(new AdminView));
+      self::$router->addController('admin_updated', new AdminUpdateController($indexView, $appConfig));
+
+      $userListController = new UserListController(new UserListView, $userRepository, $appConfig);
+      self::$router->addController('users_index', $userListController);
+      self::$router->addController('user_new', new UserNewController(new NewUserView, $userRepository));
+      self::$router->addController('user_added', new UserAddedController(new UserAddedView, $userRepository));
+      self::$router->addController('user_form_update', new UserFormController(new EditUserView, $userRepository));
+      self::$router->addController('user_updated', new UserUpdatedController(new UserUpdatedView, $userRepository));
+      self::$router->addController('user_destroy', new UserDestroyedController(new UserDestroyedView, $userRepository));
+      self::$router->addController('user_toggle_status', new UserToggleStatusController($userListController, $userRepository));
+
+      self::$router->addController('pacients_index', new PacientListController(new PacientListView($referenceDataService), $pacientsRepository, $appConfig));
+      self::$router->addController('pacient_new', new PacientNewController(new NewPacientView($referenceDataService), $pacientsRepository));
+      self::$router->addController('pacient_added', new PacientAddedController(new PacientAddedView, $pacientsRepository));
+      self::$router->addController('pacient_demographic_data', new PacientEditController(new PacientDemographicDataView($referenceDataService), $pacientsRepository));
+      self::$router->addController('pacient_form_update', new PacientEditController(new EditPacientView($referenceDataService), $pacientsRepository));
+      self::$router->addController('pacient_updated', new PacientUpdatedController(new PacientUpdatedView, $pacientsRepository));
+      self::$router->addController('pacient_destroy', new PacientDestroyedController(new PacientDestroyedView, $pacientsRepository));
+    }
+
+    return self::$router;
+  }
+}
