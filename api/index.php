bd4f21c2d0438d6e571df24bd971bdf888d2aeaa
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-27 18:08:25 -0300

    API turnos: validaciones de parametros

--- api/index.php
+++ api/index.php
@@ -4,34 +4,55 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 require_once "../vendor/autoload.php";
+require_once "validations.php";
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
 $app->get("/", function (Request $request, Response $response) {
-  $response->withStatus(200)->getBody()->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1> </body> </html>");
-  return $response;
+  return $response->withStatus(200)->withHeader('Content-Type', 'text/html')->getBody()->write(file_get_contents('index.html'));
 });
 
-$app->get("/turnos/{fecha}", function (Request $request, Response $response, $args) {
-  $response->withStatus(200)->getBody()->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1> </body> </html>");
-  $response->getBody()->write("El parametro recibido es: $args[fecha]");
-  return $response;
+$app->get("/turnos[/[{fecha}]]", function (Request $request, Response $response, $args) {
+  $body = $response->getBody();
+  $date =  $request->getAttribute('fecha', date('d-m-Y'));
+
+  if (!validateDate($date, $body))
+    return $response->withStatus(400);
+
+    $body->write("<html> <body>");
+      $body->write("<h1> Grupo 1 - API de Turnos - Proyecto de Software </h1>");
+      $body->write("La fecha recibida es: $date");
+    $body->write("</body> </html>");
+  return $response->withStatus(200);
 });
 
 $app->post("/turnos", function (Request $request, Response $response, $args) {
   $body = $response->getBody();
-  $body->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1>");
-  $body->write("Los parametros recibidos son:");
-  $body->write("<ul>");
-    $body->write("<li> DNI: " . $request->getParsedBodyParam('dni') . "</li>");
-    $body->write("<li> Fecha: " . $request->getParsedBodyParam('fecha') . "</li>");
-    $body->write("<li> Hora: " . $request->getParsedBodyParam('hora') . "</li>");
-  $body->write("</ul>");
+  $date =  $request->getParsedBodyParam('fecha', date('d-m-Y'));
+  $time = $request->getParsedBodyParam('hora');
+  $dni = $request->getParsedBodyParam('dni');
+
+  if (!validateDate($date, $body))
+    return $response->withStatus(400);
+
+    if (!validateTime($time, $body))
+    return $response->withStatus(400);
+
+  if (!validateDni($dni, $body))
+    return $response->withStatus(400);
+
+  $body->write("<html> <body>");
+    $body->write("<h1> Grupo 1 - API de Turnos - Proyecto de Software </h1>");
+    $body->write("Los parametros recibidos son:");
+    $body->write("<ul>");
+      $body->write("<li> DNI: " . $request->getParsedBodyParam('dni') . "</li>");
+      $body->write("<li> Fecha: " . $request->getParsedBodyParam('fecha') . "</li>");
+      $body->write("<li> Hora: " . $request->getParsedBodyParam('hora') . "</li>");
+    $body->write("</ul>");
   $body->write("</body> </html>");
-  $response->withStatus(200);
-  return $response;
+  return $response->withStatus(200);
 });
 
 $app->run();
