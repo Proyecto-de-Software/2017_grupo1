97841d46fe79702e54ec62b9742640b1d1f0df2b
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-26 23:50:16 -0300

    prueba API turnos

--- api/index.php
+++ api/index.php
@@ -9,8 +9,29 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello");
-    return $response;
+$app->get("/", function (Request $request, Response $response) {
+  $response->withStatus(200)->getBody()->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1> </body> </html>");
+  return $response;
 });
+
+$app->get("/turnos/{fecha}", function (Request $request, Response $response, $args) {
+  $response->withStatus(200)->getBody()->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1> </body> </html>");
+  $response->getBody()->write("El parametro recibido es: $args[fecha]");
+  return $response;
+});
+
+$app->post("/turnos", function (Request $request, Response $response, $args) {
+  $body = $response->getBody();
+  $body->write("<html> <body> <h1> Grupo 1 - API de Turnos - Proyecto de Software </h1>");
+  $body->write("Los parametros recibidos son:");
+  $body->write("<ul>");
+    $body->write("<li> DNI: " . $request->getParsedBodyParam('dni') . "</li>");
+    $body->write("<li> Fecha: " . $request->getParsedBodyParam('fecha') . "</li>");
+    $body->write("<li> Hora: " . $request->getParsedBodyParam('hora') . "</li>");
+  $body->write("</ul>");
+  $body->write("</body> </html>");
+  $response->withStatus(200);
+  return $response;
+});
+
 $app->run();
