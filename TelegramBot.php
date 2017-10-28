bda511f7771b6f2123ff10dc038fe36c9ee98535
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 19:12:17 -0300

    error path commands

--- TelegramBot.php
+++ TelegramBot.php
@@ -3,7 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '473002661:AAEsj26lXrKllYBfe5JXHphdHWYxX41dfjA';
 $bot_username = 'grupo1_proyecto2017_bot';
-$commands_paths = [__DIR__ . 'private/Telegram/Commands'];
+$commands_paths = [__DIR__ . '/private/Telegram/Commands'];
 try
 {
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
