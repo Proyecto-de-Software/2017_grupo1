fba64a28886d0b5917b5bde75f37c95bf22963f0
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 20:15:10 -0300

    prueba implementacion comando /turnos

--- private/Telegram/Commands/TurnosCommand.php
+++ private/Telegram/Commands/TurnosCommand.php
@@ -4,24 +4,51 @@ namespace Longman\TelegramBot\Commands\UserCommands;
 use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Request;
 
+require_once "/private/Telegram/Commands/autoloader.php";
+
 class TurnosCommand extends UserCommand
 {
-    protected $name = 'turnos';
-    protected $description = '/turnos dd-mm-aaaa: Devolverá los turnos disponibles para la fecha indicada';
-    protected $usage = '/turnos';
-    protected $version = '1.0.0';
+    private $name = 'turnos';
+    private $description = '/turnos dd-mm-aaaa: Devolverá los turnos disponibles para la fecha indicada';
+    private $usage = '/turnos';
+    private $version = '1.0.0';
+
+    private function isValidDate($text)
+    {
+      $d = DateTime::createFromFormat('d-m-Y', $text);
+      return ($d && $d->format('d-m-Y') == $text);
+    }
+
+    private function getRepository()
+    {
+      return new AppointmentsRepository();
+    }
+
+    private function getAppointments($date)
+    {
+      return print_r($this->getRepository()->getAppointments($date));
+    }
 
     public function execute()
     {
       $message = $this->getMessage();
-      $fecha = $message->getText(true);
-
       $chat_id = $message->getChat()->getId();
+      $date = $message->getText(true);
 
-      $data = [
-      'chat_id' => $chat_id,
-      'text' => "este es el comando /turnos, con parametro $fecha"
-      ];
+      if (!$this->isValidDate($date))
+      {
+        $data = [
+          'chat_id' => $chat_id,
+          'text' => "$date no es una fecha valida, usar formato dd-mm-aaaa. Ejemplo <25-10-2017>"
+          ];
+      }
+      else
+      {
+        $data = [
+          'chat_id' => $chat_id,
+          'text' => $this->getAppointments($date)
+          ];
+      }
 
       return Request::sendMessage($data);
     }
---
+++ private/Telegram/Commands/autoloader.php
@@ -0,0 +1,4 @@
+<?php
+require_once "/private/models/PDORepository.php";
+require_once "/api/Appointment.php";
+require_once "/api/AppointmentsRepository.php";
