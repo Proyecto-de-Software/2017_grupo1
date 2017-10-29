5fe352b372b328834174ca1262b2b6d2a57970c0
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 22:19:45 -0300

    error tipeo

--- TelegramBot.php
+++ TelegramBot.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . 'private/Telegram/Commands/autoloader.php';
+require __DIR__ . '/private/Telegram/Commands/autoloader.php';
 require  './private/Telegram/bot_config.php';
 
 $commands_paths = [__DIR__ . '/private/Telegram/Commands'];
