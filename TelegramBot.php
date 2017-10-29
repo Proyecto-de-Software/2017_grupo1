4e3233f470cec549d7d09819af18aa17cccefebc
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 21:19:55 -0300

    saco el logging de excepciones (seguro no me deja crear archivos en el server)

--- TelegramBot.php
+++ TelegramBot.php
@@ -7,14 +7,11 @@ try
 {
     $telegram = new Longman\TelegramBot\Telegram(TelegramBotConfig::bot_api_key, TelegramBotConfig::bot_username);
     // Logging (Error, Debug and Raw Updates)
-    Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/bot_error.log");
-    Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/bot_debug.log");
-    Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/bot_update.log");
     $telegram->addCommandsPaths($commands_paths);
     $telegram->enableLimiter();
     $telegram->handle();
 }
 catch (Longman\TelegramBot\Exception\TelegramException $e)
 {
-  Longman\TelegramBot\TelegramLog::error($e);
+  // controlar excepcion
 }
