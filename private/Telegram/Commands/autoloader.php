6558d29ce80078f1c6d26d926ff753538fbbec56
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 22:22:53 -0300

    errores de path en el autoloader de telegram

--- private/Telegram/Commands/autoloader.php
+++ private/Telegram/Commands/autoloader.php
@@ -1,4 +1,4 @@
 <?php
-require_once '../../models/PDORepository.php';
-require_once '../../../api/Appointment.php';
-require_once '../../../api/AppointmentsRepository.php';
+require_once 'private/models/PDORepository.php';
+require_once 'api/Appointment.php';
+require_once 'api/AppointmentsRepository.php';
