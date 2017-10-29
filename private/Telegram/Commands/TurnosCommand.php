b76fa28918e55fdb018bae20cabbf75bb5aaa324
Agustin Ortu <ortu.agustin@gmail.com>
2017-10-28 21:53:23 -0300

    manejador de excepciones en turnos command

--- private/Telegram/Commands/TurnosCommand.php
+++ private/Telegram/Commands/TurnosCommand.php
@@ -6,41 +6,50 @@ use Longman\TelegramBot\Request;
 
 class TurnosCommand extends UserCommand
 {
-    protected $name = 'turnos';
-    protected $description = '/turnos dd-mm-aaaa: Devolverá los turnos disponibles para la fecha indicada';
-    protected $usage = '/turnos';
-    protected $version = '1.0.0';
+  protected $name = 'turnos';
+  protected $description = '/turnos dd-mm-aaaa: Devolverá los turnos disponibles para la fecha indicada';
+  protected $usage = '/turnos';
+  protected $version = '1.0.0';
 
-    private function isValidDate($text)
-    {
-      $d = DateTime::createFromFormat('d-m-Y', $text);
-      if (!($d && $d->format('d-m-Y') == $text))
-        return false;
+  private function isValidDate($text)
+  {
+    $d = DateTime::createFromFormat('d-m-Y', $text);
+    if (! ($d && $d->format('d-m-Y') == $text))
+      return false;
 
-      return true;
-    }
+    return true;
+  }
 
-    public function execute()
-    {
+  public function execute()
+  {
     $message = $this->getMessage();
     $chat_id = $message->getChat()->getId();
     $date = $message->getText(true);
 
-    if (!$this->isValidDate($date))
+    try
+      {
+      if (!$this->isValidDate($date))
+        {
+        $data = [
+          'chat_id' => $chat_id,
+          'text' => "$date no es una fecha valida, usar formato dd-mm-aaaa. Ejemplo <25-10-2017>"
+        ];
+      }
+      else
+        {
+        $data = [
+          'chat_id' => $chat_id,
+          'text' => "la fecha es $date"
+        ];
+      }
+    } catch (Exception $e)
       {
       $data = [
         'chat_id' => $chat_id,
-        'text' => "$date no es una fecha valida, usar formato dd-mm-aaaa. Ejemplo <25-10-2017>"
-      ];
-    }
-    else
-    {
-      $data = [
-        'chat_id' => $chat_id,
-        'text' => "la fecha es $date"
+        'text' => e . getMessage()
       ];
     }
 
-      return Request::sendMessage($data);
-    }
+    return Request::sendMessage($data);
+  }
 }
